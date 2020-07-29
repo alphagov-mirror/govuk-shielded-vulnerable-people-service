6de8cca67052edbc7def22889dcdd33393c4bc3e
Dan Williams <dan@ithium.net>
2020-07-29 17:40:15 +0100

    Moved session persistence to be before validation
    This will allow validation to draw it's answers from the session

--- vulnerable_people_form/views.py
+++ vulnerable_people_form/views.py
@@ -240,9 +240,9 @@ def validate_applying_on_own_behalf():
 
 @form.route("/applying-on-own-behalf", methods=["POST"])
 def post_applying_on_own_behalf():
+    update_session_answers_from_form()
     if not validate_applying_on_own_behalf():
         return redirect("/applying-on-own-behalf")
-    update_session_answers_from_form()
     return route_to_next_form_page()
 
 
@@ -266,9 +266,9 @@ def get_nhs_login():
 
 @form.route("/nhs-login", methods=["POST"])
 def post_nhs_login():
+    update_session_answers_from_form()
     if not validate_nhs_login():
         return redirect("/nhs-login")
-    update_session_answers_from_form()
     return route_to_next_form_page()
 
 
@@ -333,9 +333,9 @@ def get_live_in_england():
 
 @form.route("/live-in-england", methods=["POST"])
 def post_live_in_england():
+    update_session_answers_from_form()
     if not validate_live_in_england():
         return redirect("/live-in-england")
-    update_session_answers_from_form()
     return route_to_next_form_page()
 
 
@@ -354,9 +354,9 @@ def validate_nhs_letter():
 
 @form.route("/nhs-letter", methods=["POST"])
 def post_nhs_letter():
+    update_session_answers_from_form()
     if not validate_nhs_letter():
         return redirect("/nhs-letter")
-    update_session_answers_from_form()
     return route_to_next_form_page()
 
 
@@ -388,10 +388,9 @@ def validate_medical_conditions():
 
 @form.route("/medical-conditions", methods=["POST"])
 def post_medical_conditions():
+    update_session_answers_from_form()
     if not validate_medical_conditions():
         return redirect("/medical-conditions")
-
-    update_session_answers_from_form()
     return route_to_next_form_page()
 
 
@@ -856,9 +855,9 @@ def get_essential_supplies():
 
 @form.route("/essential-supplies", methods=["POST"])
 def post_essential_supplies():
+    update_session_answers_from_form()
     if not validate_essential_supplies():
         return redirect("/essential-supplies")
-    update_session_answers_from_form()
     return route_to_next_form_page()
 
 
@@ -890,9 +889,9 @@ def get_basic_care_needs():
 
 @form.route("/basic-care-needs", methods=["POST"])
 def post_basic_care_needs():
+    update_session_answers_from_form()
     if not validate_basic_care_needs():
         return redirect("/basic-care-needs")
-    update_session_answers_from_form()
     return route_to_next_form_page()
 
 
@@ -918,9 +917,9 @@ def get_dietary_requirements():
 
 @form.route("/dietary-requirements", methods=["POST"])
 def post_dietary_requirements():
+    update_session_answers_from_form()
     if not validate_dietary_requirements():
         return redirect("/dietary-requirements")
-    update_session_answers_from_form()
     return route_to_next_form_page()
 
 
@@ -946,9 +945,9 @@ def get_carry_supplies():
 
 @form.route("/carry-supplies", methods=["POST"])
 def post_carry_supplies():
+    update_session_answers_from_form()
     if not validate_carry_supplies():
         return redirect("/carry-supplies")
-    update_session_answers_from_form()
     return route_to_next_form_page()
 
 
